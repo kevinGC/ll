f52de37e2b9d17851ea6587aa5653a401ad554d7
Kevin Krakauer <krakauer@umich.edu>
2019-07-21 19:30:42 -0700

    Added title and split games.js out.

--- ll.js
+++ ll.js
@@ -1,15 +1,3 @@
-// A record of every game from which to build the tables.
-var games = [
-	{ home: "Matt", away: "Kevin", homeScore: 10, awayScore: 0, date: "07/21/2019", freakouts: 0 },
-	{ home: "Matt", away: "Kevin", homeScore: 1, awayScore: 5, date: "07/21/2019", freakouts: 0 },
-	{ home: "Kevin", away: "Matt", homeScore: 3, awayScore: 2, date: "07/21/2019", freakouts: 0 },
-	{ home: "Kevin", away: "Ryan", homeScore: 4, awayScore: 3, date: "07/20/2019", freakouts: 0 },
-	{ home: "Ryan", away: "Kevin", homeScore: 8, awayScore: 2, date: "07/20/2019", freakouts: 1 },
-	{ home: "Ryan", away: "Kevin", homeScore: 3, awayScore: 2, date: "07/19/2019", freakouts: 0 },
-	{ home: "Ryan", away: "Kevin", homeScore: 4, awayScore: 7, date: "07/19/2019", freakouts: 1 },
-	{ home: "Kevin", away: "Ryan", homeScore: 5, awayScore: 3, date: "07/19/2019", freakouts: 0 },
-];
-
 // A list of players with their wins and losses.
 var winsLosses = [];
 // A map of player names to index in winsLosses.
